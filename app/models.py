5cf549f7adcf392b40848fd2d51422285d34eb6d
roman-ojha <razzroman98@gmail.com>
2023-09-05 13:32:27 +0545

    feat: added Book & Order Model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    slug = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image_url = models.TextField()
+
+class Order(models.Model):
+    total = models.IntegerField()
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    is_completed = models.BooleanField(default=False)
